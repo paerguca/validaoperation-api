029da7a7c6d202919a35c3095bf13341aabc9ce9
Paul Gutierrez <paerguca@gmail.com>
2018-12-15 07:39:23 -0500

    Quitando configuracion de acceso CORS

--- app.js
+++ app.js
@@ -19,16 +19,16 @@ app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+//app.use((req, res, next) => {
+  //res.header('Access-Control-Allow-Origin', '*')
+  //res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
   
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-  res.header('Access-Control-Allow-Credentials', true)
+  //res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  //res.header('Access-Control-Allow-Credentials', true)
 
   
-  next()
-})
+  //next()
+//})
 
 app.use('/operaciones', operacionesRoutes)
 
