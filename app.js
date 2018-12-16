7de2b5c68051faef28006d73acc9e6f968f9dc00
Paul Gutierrez <paerguca@gmail.com>
2018-12-15 22:15:02 -0500

    API con soporte para deploy

--- app.js
+++ app.js
@@ -35,4 +35,13 @@ app.use(cors())
 const operacionesRoutes = require('./api/routes/operaciones')
 app.use('/operaciones', operacionesRoutes)
 
+// Handle production
+if (process.env.NODE_ENV == 'production') {
+  // Static folder
+  app.use(express.static(__dirname + '/public/'))
+
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+}
+
 module.exports = app
